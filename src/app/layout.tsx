7c9bfe63f3475dc08cba41613350bdbbe4fa61de
Luiz Felipe Olivieri <83732763+luizfelipeolivieri@users.noreply.github.com>
2026-03-24 19:01:18 -0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'MBX Login',
+  description: 'Projeto com Next.js + Supabase',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
