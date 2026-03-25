4239c8240ec1c6158e63f4b210e682bc1a44a633
Luiz Felipe Olivieri <83732763+luizfelipeolivieri@users.noreply.github.com>
2026-03-25 17:58:07 -0300

    Create chartData.ts

---
+++ utils/chartData.ts
@@ -0,0 +1,38 @@
+export const areaData = [
+  { month: "Jan", receita: 800, despesas: 500 },
+  { month: "Fev", receita: 900, despesas: 600 },
+  { month: "Mar", receita: 700, despesas: 650 },
+  { month: "Abr", receita: 1000, despesas: 700 },
+  { month: "Mai", receita: 1200, despesas: 800 },
+  { month: "Jun", receita: 1100, despesas: 750 },
+  { month: "Jul", receita: 1300, despesas: 900 },
+  { month: "Ago", receita: 1400, despesas: 950 },
+  { month: "Set", receita: 1500, despesas: 1000 },
+  { month: "Out", receita: 1600, despesas: 1100 },
+  { month: "Nov", receita: 1700, despesas: 1200 },
+  { month: "Dez", receita: 2000, despesas: 1300 }
+];
+
+export const gastosMes = [
+  { name: "Supermercado", value: 400 },
+  { name: "Restaurante", value: 200 },
+  { name: "Utilidades", value: 150 },
+  { name: "Moradia", value: 300 },
+  { name: "Educação", value: 100 },
+  { name: "Saúde", value: 80 },
+  { name: "Transporte", value: 120 },
+  { name: "Lazer", value: 90 },
+  { name: "Presentes", value: 60 }
+];
+
+export const gastosAno = [
+  { name: "Supermercado", value: 4800 },
+  { name: "Restaurante", value: 2400 },
+  { name: "Utilidades", value: 1800 },
+  { name: "Moradia", value: 3600 },
+  { name: "Educação", value: 1200 },
+  { name: "Saúde", value: 900 },
+  { name: "Transporte", value: 1500 },
+  { name: "Lazer", value: 1100 },
+  { name: "Presentes", value: 800 }
+];
