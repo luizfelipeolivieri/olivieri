970127dfc0cab50d4085638c6104f3d77033f132
Luiz Felipe Olivieri <83732763+luizfelipeolivieri@users.noreply.github.com>
2026-03-25 18:08:14 -0300

    Create marketStructure.ts

---
+++ utils/marketStructure.ts
@@ -0,0 +1,21 @@
+export const marketStructure = [
+  {
+    categoria: "Alimentos",
+    produtos: [
+      { nome: "Arroz", quantidade: "5kg" },
+      { nome: "Feijão", quantidade: "1kg" }
+    ]
+  },
+  {
+    categoria: "Bebidas",
+    produtos: [
+      { nome: "Refrigerante", quantidade: "2L" }
+    ]
+  },
+  {
+    categoria: "Higiene",
+    produtos: [
+      { nome: "Shampoo", quantidade: "400ml" }
+    ]
+  }
+];
