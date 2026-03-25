c02a171885695cc244b5304239fee3e4595759b4
Luiz Felipe Olivieri <83732763+luizfelipeolivieri@users.noreply.github.com>
2026-03-25 17:55:02 -0300

    Create useAuth.ts

---
+++ hooks/useAuth.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { supabase } from "@/services/supabaseClient";
+
+export function useAuth() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return { user };
+}
